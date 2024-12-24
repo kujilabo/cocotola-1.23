8bec3959fc4e93cfc3135f7bbb2f2294e1339cc4
pecolynx <pecolynx@gmail.com>
2024-12-24 19:06:58 +0900

    refactor

--- redstart/lib/service/transaction_manager.go
+++ redstart/lib/service/transaction_manager.go
@@ -22,3 +22,17 @@ func Do1[RF any, T1 any](ctx context.Context, txManager TransactionManagerT[RF],
 	})
 	return t1, err
 }
+
+func Do2[RF any, T1, T2 any](ctx context.Context, txManager TransactionManagerT[RF], fn func(rf RF) (T1, T2, error)) (T1, T2, error) {
+	var t1 T1
+	var t2 T2
+	err := txManager.Do(ctx, func(rf RF) error {
+		var t1tmp T1
+		var t2tmp T2
+		t1tmp, t2tmp, err := fn(rf)
+		t1 = t1tmp
+		t2 = t2tmp
+		return err
+	})
+	return t1, t2, err
+}
