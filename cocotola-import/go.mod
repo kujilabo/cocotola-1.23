b6da31510ec3be794b05f8169eb791514225deb4
pecolynx <pecolynx@gmail.com>
2025-02-03 00:24:55 +0900

    fix

--- cocotola-import/go.mod
+++ cocotola-import/go.mod
@@ -3,22 +3,56 @@ module github.com/kujilabo/cocotola-1.23/cocotola-import
 go 1.23.3
 
 require (
+	firebase.google.com/go/v4 v4.15.1
 	github.com/kujilabo/cocotola-1.23/redstart v0.0.0-20250105063647-e108d1051af6
+	google.golang.org/api v0.214.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	cloud.google.com/go v0.112.2 // indirect
+	cloud.google.com/go/auth v0.13.0 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.6 // indirect
+	cloud.google.com/go/compute/metadata v0.6.0 // indirect
+	cloud.google.com/go/firestore v1.15.0 // indirect
+	cloud.google.com/go/iam v1.1.7 // indirect
+	cloud.google.com/go/longrunning v0.5.6 // indirect
+	cloud.google.com/go/storage v1.40.0 // indirect
+	github.com/MicahParks/keyfunc v1.9.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.23.0 // indirect
-	github.com/kr/pretty v0.3.1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/s2a-go v0.1.8 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
+	github.com/googleapis/gax-go/v2 v2.14.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.58.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.58.0 // indirect
+	go.opentelemetry.io/otel v1.33.0 // indirect
+	go.opentelemetry.io/otel/metric v1.33.0 // indirect
+	go.opentelemetry.io/otel/trace v1.33.0 // indirect
 	golang.org/x/crypto v0.32.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/oauth2 v0.24.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/time v0.8.0 // indirect
 	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	google.golang.org/appengine/v2 v2.0.2 // indirect
+	google.golang.org/genproto v0.0.0-20240213162025-012b6fc9bca9 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250102185135-69823020774d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250102185135-69823020774d // indirect
+	google.golang.org/grpc v1.69.2 // indirect
+	google.golang.org/protobuf v1.36.1 // indirect
 )
--- cocotola-import/main.go
+++ cocotola-import/main.go
@@ -1,9 +1,139 @@
 package main
 
-import "github.com/kujilabo/cocotola-1.23/cocotola-import/tatoeba"
+import (
+	"context"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path"
+
+	firebase "firebase.google.com/go/v4"
+	"github.com/kujilabo/cocotola-1.23/cocotola-import/tatoeba"
+	"google.golang.org/api/option"
+)
 
 func main() {
-	if err := tatoeba.ImportTatoebaEngSentences(); err != nil {
+	ctx := context.Background()
+	if err := fireabase(ctx); err != nil {
+		log.Fatal(fmt.Sprintf("fireabase error: %+v", err))
 		panic(err)
 	}
+}
+
+func db() error {
+	if err := tatoeba.ImportTatoebaEngSentences(); err != nil {
+		return err
+	}
+	return nil
+}
+
+type Iterator interface {
+	Next(ctx context.Context) ([]string, error)
+	HasNext() bool
+}
+
+type CSVReader struct {
+	reader *csv.Reader
+	words  []string
+	eof    bool
+	err    error
+}
+
+func NewCSVReader(reader io.Reader) Iterator {
+	csvReader := csv.NewReader(reader)
+	csvReader.Comma = '\t'
+	csvReader.LazyQuotes = true
+
+	eof := false
+	words, err := csvReader.Read()
+	if err == io.EOF {
+		eof = true
+	}
+	return &CSVReader{
+		reader: csvReader,
+		words:  words,
+		eof:    eof,
+		err:    err,
+	}
+}
+
+func (r *CSVReader) Next(ctx context.Context) ([]string, error) {
+	if r.err != nil {
+		return nil, r.err
+	}
+	words := r.words
+	err := r.err
+	r.words, r.err = r.reader.Read()
+	if r.err == io.EOF {
+		r.eof = true
+	}
+	return words, err
+}
+
+func (r *CSVReader) HasNext() bool {
+	return !r.eof
+}
+
+func ProcessLine(ctx context.Context, itr Iterator, fn func([]string) error) error {
+	for itr.HasNext() {
+		words, err := itr.Next(ctx)
+		if err != nil {
+			return err
+		}
+		if err := fn(words); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func fireabase(ctx context.Context) error {
+	// Firebase Develop 管理者
+	// Firebase Develop Admin
+	sa := option.WithCredentialsFile("../keys/firestore-importer.serviceAccount.json")
+	// sa := option.WithCredentialsFile("../keys/firestore-adminsdk.serviceAccount.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		return err
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	filePath := path.Join("data", "english-word-problem-001.csv")
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	itr := NewCSVReader(file)
+	if err := ProcessLine(ctx, itr, func(words []string) error {
+		fmt.Println(words)
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	// _, err = client.Collection("users").Doc("aaa").Set(ctx, map[string]interface{}{
+	// 	"id":         "sampleId",
+	// 	"userName":   "go master",
+	// 	"email":      "go.master@exsample.com",
+	// 	"technology": "bbb",
+	// })
+
+	// // データ追加
+	// docs, err := client.Collection("users").Documents(ctx).GetAll()
+	// if err != nil {
+	// 	return err
+	// }
+	// for _, doc := range docs {
+	// 	log.Println(doc.Data())
+	// }
+	return nil
 }
